ba32112236cc8cbc10584b2a3089bf55c694b13a
Zsolt Klauzer <99751489+zsklz@users.noreply.github.com>
2023-08-04 14:07:46 +0200

    Draw 16x16 grid with DOM manipulation

--- sketch.js
+++ sketch.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+// generate the 16x16 grid (with flexbox css)
+const container = document.querySelector('#app');
+for (let i = 0; i < 16; ++i) {
+    const row = document.createElement('div');
+    row.classList.toggle('row');
+    for (let j = 0; j < 16; ++j) {
+	const gridSquare = document.createElement('div');
+	gridSquare.classList.toggle('gridsquare');
+	row.appendChild(gridSquare);
+    }
+    container.appendChild(row);
+}
